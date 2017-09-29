0a0b2d62f8237871bce9f04e3c809b829e89a822
Zainab-Kalief <alesewura@yahoo.com>
2017-09-29 03:06:11 -0700

    changes

--- server.js
+++ server.js
@@ -1,42 +1,68 @@
-var express = require('express')
-var path = require('path')
-var app = express()
-var body_parser = require('body-parser')
-var path = require('path')
+//***********//
+//~~~~~~SET UP~~~~~~//
+const express = require('express')
+const path = require('path')
+const app = express()
+const body_parser = require('body-parser')
+const path = require('path')
 app.use(body_parser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, './static')))
 app.use(body_parser.json())
+//**********//
 
-var users = []
-var db = []
-var flipGame = true
-var dbCopy = []
-//route
-app.get('/stat', (request, response) => {
-  response.json(users)
+
+//**********//
+//~~~~~~DATA STORAGE~~~~~~//
+var players = [] //creates players as they add thier join game
+var db = [] //main db
+var gameStatus = true //shuts off others from joining the game once it has began
+var dbCopy = [] //used to store db before it resets
+
+//~~~~~~backend admin variables~~~~~~//
+var gameLogs = []
+var gameTotalRaw = {}
+var gameTotalFormatted = []
+//**********//
+
+
+//**********//
+//~~~~~~API CALLS~~~~~~//
+app.get('/stat', (request, response) => { //from backend admin
+  gameTotalFormatted = []
+  for (var player in gameTotalRaw) {
+    gameTotalFormatted.push({name: player, score: gameTotalRaw[player]})
+  }
+  gameTotalFormatted.sort( (a, b) => {
+    return b.score - a.score
+  })
+  response.json({team: players, status: gameStatus, logs: gameLogs, total: gameTotalFormatted})
 })
-app.get('/all', (request, response) => {
-  console.log(dbCopy);
+
+app.get('/all', (request, response) => { //from client - iOS
   response.json({data: dbCopy})
 })
-app.get('/admin', (request, response) => {
-  response.json({data: users[0]})
+app.get('/admin', (request, response) => { //from client - iOS
+  response.json({data: players[0]})
 })
+//**********//
 
 
+//~~~~~~SERVER CONNECTION~~~~~~//
+const server = app.listen(8000, () => { console.log('listening on port  8000'); })
 
-var server = app.listen(8000, () => { console.log('listening on port  8000'); })
-//socket
-var io = require('socket.io').listen(server)
+
+//**********//
+//~~~~~~SOCKET IO~~~~~~//
+const io = require('socket.io').listen(server)
 io.sockets.on('connection', function (socket) {
 
-
+  //1 - players join game
   socket.on('joinGame', function (data) {
-    if(flipGame) {
-      if(!users.includes(data)) {
-        users.push(data)
+    if(gameStatus) {  //preventing additional players from being added after the game has started
+      if(!players.includes(data)) {
+        players.push(data)
       }
-      io.emit('allUsers', users)
+      io.emit('allUsers', players)
       console.log('User joined chat')
     } else {
       console.log('access denied!!!');
@@ -44,55 +70,67 @@ io.sockets.on('connection', function (socket) {
   })
 
 
+  //2 - admin player starts the game
   socket.on("startGame", function (time) {
-    for (var i = 0; i < users.length; i++) {
+    gameStatus = false
+
+    for (var i = 0; i < players.length; i++) { //create the db
       if(i == 0) {
-        db.push({name: users[i], targets: {}, status: true})
+        db.push({name: players[i], targets: {}, status: true}) //establish admin player who will also be the first user
       } else {
-        db.push({name: users[i], targets: {}, status: false})
+        db.push({name: players[i], targets: {}, status: false})
       }
+      gameTotalRaw[players[i]] = 0
     }
+
     for (user of db) {
-      for (val of users) {
-        if (val != user.name) { user.targets[val] = 0   }
+      for (val of players) {
+        if (val != user.name) { user.targets[val] = 0  } //create targets
       }
     }
-    flipGame = false
+
     console.log('Users just started game')
     io.emit("beginGame", db)
-    // setTimeout( () => {
-    //   io.emit('timeWarning', 60)
-    // }, 80)
+
+    setTimeout( () => { //first warning
+      io.emit('timeWarning', 60)
+      console.log('60 seconds left');
+    }, Number(time) - 60000)
+    setTimeout( () => { //second warning
+      io.emit('timeWarning', 10)
+      console.log('10 seconds left');
+    }, Number(time) - 10000)
     setTimeout(function () { //end game
       io.emit("gameOver", db)
       console.log("End the game after this time");
       dbCopy = db
-      users = []
+      players = []
       db = []
-      flipGame = true
-    }, Number(time))
+      gameStatus = true
+    }, Number(time)) //the admin player determines when the game ends
+
   })
 
 
+  //3 players fire shots
   socket.on("shotsFired", function (data) {
     for (user of db) {
       if(user.name == data.shooter) {
-        var temp = user
-        if (data.target in user.targets) {
+        if (data.target in user.targets) { //record shots fired
           user.targets[data.target] += 1
           io.emit("target", data.target)
         }
+        //if player shoots themselves
+        if (!(data.target in user.targets)) {
+          io.emit("idiotShot", data.target)
+        }
       }
     }
-    if (!(data.target in temp.targets)) {
-      io.emit("idiotShot", data.target)
-    }
+
+    //updating backend admin
+    gameLogs.push(`${data.shooter} shot ${data.target}`)
+    gameTotalRaw[data.shooter] += 1
   })
-
-  socket.on('sendResult', function () {
-    io.emit('receiveResult', db)
-
-  })
-
 
 })
+//**********//
